95c2a3e2064a02754b995ca616e5cbc09734514d
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-01 14:34:39 +0200

    [DB Context] Add datbase context using EF.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("ReactivityContext"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
