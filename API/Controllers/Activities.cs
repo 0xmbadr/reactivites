390d1dee42e80067718c243c83e6705a03820883
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-02 21:29:35 +0200

    [API] Add Details class to handle read single activity from DB.

--- API/Controllers/Activities.cs
+++ API/Controllers/Activities.cs
@@ -17,7 +17,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            // Object Initializer Syntax 
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
