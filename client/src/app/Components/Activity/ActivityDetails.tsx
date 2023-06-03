e51f6d5b6cabf2fe46063f67f834a58bbd06b888
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-03 15:13:30 +0200

    [client] Add activityDetails component to show details of a specific activity.

--- client/src/app/Components/Activity/ActivityDetails.tsx
+++ client/src/app/Components/Activity/ActivityDetails.tsx
@@ -1,11 +1,32 @@
 import { Activity } from '@/app/@types/Activity';
+import { Button, ButtonGroup, Card, Image } from 'semantic-ui-react';
 
 interface ActivityDetailsProps {
   activity: Activity;
 }
 
 function ActivityDetails({ activity }: ActivityDetailsProps) {
-  return <div>ActivityDetails</div>;
+  return (
+    <Card fluid>
+      <Image
+        src={`/categoryImages/${activity.category}.jpg`}
+        alt={`${activity.category} image`}
+      />
+      <Card.Content>
+        <Card.Header>{activity.category}</Card.Header>
+        <Card.Meta>
+          <span>{activity.dateTime.toString()}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <ButtonGroup widths="2">
+          <Button basic color="blue" content="Edit"></Button>
+          <Button basic color="grey" content="Cancel"></Button>
+        </ButtonGroup>
+      </Card.Content>
+    </Card>
+  );
 }
 
 export default ActivityDetails;
