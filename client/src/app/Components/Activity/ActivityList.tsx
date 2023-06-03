0d7f0d9d04da68757fce594b01a36466ea65b8b3
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-03 15:09:19 +0200

    [client] add activityList component to show list of all activities.

--- client/src/app/Components/Activity/ActivityList.tsx
+++ client/src/app/Components/Activity/ActivityList.tsx
@@ -1,11 +1,35 @@
 import { Activity } from '@/app/@types/Activity';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
 
 interface ActivityListProps {
   activities: Activity[];
 }
 
 function ActivityList({ activities }: ActivityListProps) {
-  return <div>ActivityList</div>;
+  return (
+    <Segment>
+      <Item.Group divided>
+        {activities.map((activity: Activity) => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Meta>{activity.dateTime.toString()}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button floated="right" content="View" color="blue"></Button>
+                <Label basic content={activity.category}></Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
 }
 
 export default ActivityList;
