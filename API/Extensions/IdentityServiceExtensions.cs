1f431912d3f57515d5c58434f74b202081a06585
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-15 14:15:06 +0200

    [API] Add and configure Idenitity.

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,26 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            services
+                .AddIdentityCore<AppUser>(options =>
+                {
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                .AddEntityFrameworkStores<DataContext>()
+                .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
