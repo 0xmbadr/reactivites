5d4003e9227319a92f89dc14ebc3b35464b53eef
MrCarbene <Muhammad_Fouda@proton.me>
2023-06-19 20:47:39 +0200

    [Relationships] Add Many-to-Many Relationship between AppUser and Activity.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,5 +10,28 @@ namespace Persistence
             : base(options) { }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendees> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendees>(
+                // Specifying primary key
+                aa => aa.HasKey(aa => new { aa.AppUserId, aa.ActivityId })
+            );
+
+            builder
+                .Entity<ActivityAttendees>()
+                .HasOne(aa => aa.AppUser)
+                .WithMany(user => user.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder
+                .Entity<ActivityAttendees>()
+                .HasOne(aa => aa.Activity)
+                .WithMany(activity => activity.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
